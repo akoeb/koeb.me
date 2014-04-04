93e3f5e2d260378b2258ec17c7b2b7402013f183
Alexander Köb <nerdkram@koeb.me>
2014-04-04 11:39:02 +0200

    creating and viewing blog posts

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,99 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
+// need that for Pk datatype
+import anorm._
+
+import models._
+import views._
+
+
+// do not re-use model case class, because we have id and date in model
+//case class BlogPost (title: String, teaser: String, text: String)
+
+
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+    val blogPostForm = Form(
+        mapping(
+            "id" -> ignored(NotAssigned:Pk[Long]),
+            "title" -> nonEmptyText,
+            "teaser" -> optional(text),
+            "text" -> nonEmptyText,
+            "created" -> optional(date)  // TODO: how to make that ignored?
+        )(Post.apply)(Post.unapply)
+    )
+
+// Homepage action
+    def index = Action {
+        Redirect(routes.Application.listPosts(0,10))
+    }
+
+    // ACTIONS
+
+    /**
+     * Display the paginated list of posts
+     *
+     * @param page Current page number (starts from 0)
+     * @param postsPerPage number of posts on one page
+     */
+    def listPosts(page: Int, postsPerPage: Int) = Action { implicit request =>
+        Ok(html.list(Post.list(page = page, postsPerPage = postsPerPage)))
+    }
+
+    /**
+     * display form to create a new post
+     *
+     */
+    def createPost = Action {
+        Ok(html.createForm(blogPostForm))
+    }
+
+    /**
+     * save a new Post to the persistence backend
+     *
+     * @param title the post headline 
+     * @param teaser the teaser text of the post
+     * @param text   the post body
+     */
+    def savePost = Action { implicit request =>
+        blogPostForm.bindFromRequest.fold(
+            formWithErrors => BadRequest(html.createForm(formWithErrors)),
+            blogPost => {
+                /* binding success, you get the actual value. */
+                //val newPost = Post(blogPost.title, blogPost.teaser, blogPost.text)
+                Post.insert(blogPost)
+                Redirect(routes.Application.index).flashing("success" -> "Post %s has been created".format(blogPost.title))
+            }
+        )
+    }
+
+    /**
+     * display form to edit an existing blog post
+     *
+     * @param id the post id
+     */
+    def editPost(id: Long) = TODO
+
+    /**
+     * save changes to an existing post
+     * 
+     * @param id the post id
+     * @param title the post headline 
+     * @param teaser the teaser text of the post
+     * @param text   the post body
+     */
+    def updatePost(id: Long) = TODO
+
+    /**
+     * delete a post
+     *
+     * @param id the post id
+     */
+    def deletePost(id: Long) = TODO
+
 
 }
---
+++ app/models/Post.scala
@@ -0,0 +1,67 @@
+package models
+
+import play.api.db._
+import play.api.Play.current
+
+import anorm._
+import anorm.SqlParser._
+
+import java.util.{Date}
+
+
+case class Post(id: Pk[Long] = NotAssigned, title: String, teaser: Option[String], text: String, created: Option[Date] )
+
+
+object Post {
+
+    // parser for sql queries:
+    val post = {
+        get[Pk[Long]]("id") ~
+        get[String]("title") ~
+        get[Option[String]]("teaser") ~
+        get[String]("text") ~
+        get[Option[Date]]("created") map {
+            case id~title~teaser~text~created => Post(id, title, teaser, text, created)
+        }
+    }
+
+    /*
+    * model to return a paginated list of posts
+    *
+    * @param page The page number, 0 to start
+    * @param postsPerPage number of posts we want to display on each page
+    */
+    def list(page: Int, postsPerPage: Int): List[Post] = {
+        val offset = page * postsPerPage
+        DB.withConnection { implicit conn =>
+            SQL("SELECT * from posts order by created desc limit {postsPerPage} offset {offset}").on(
+                'offset -> offset,
+                'postsPerPage -> postsPerPage
+            ).as(post *)
+        }
+    }
+
+    /**
+     * Insert a new computer.
+     *
+     * @param computer The computer values.
+     */
+    def insert(post: Post) = {
+        DB.withConnection { implicit connection =>
+            SQL("""INSERT INTO posts (id, title, teaser, text, created)
+                VALUES (
+                    (select next value for post_id_seq), 
+                    {title}, {teaser}, {text}, now()
+                )""").on(
+                'title -> post.title,
+                'teaser -> post.teaser,
+                'text -> post.text
+            ).executeUpdate()
+        }
+    }
+
+
+    def delete(id: Long) {}
+
+
+}
